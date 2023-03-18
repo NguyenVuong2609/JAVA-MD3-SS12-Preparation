47c0471afb16a60432252108ef9a2875f5cf5b98
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2023-03-18 16:26:08 +0700

    1803

---
+++ src/Rikkei/academy/TH4_SearchBinary/AbstractTree.java
@@ -0,0 +1,8 @@
+package Rikkei.academy.TH4_SearchBinary;
+
+public abstract class AbstractTree<E> implements Tree<E> {
+    @Override /** Inorder traversal from the root*/
+    public void inorder() {
+    }
+
+}
---
+++ src/Rikkei/academy/TH4_SearchBinary/TreeNode.java
@@ -0,0 +1,11 @@
+package Rikkei.academy.TH4_SearchBinary;
+
+public class TreeNode<E> {
+    protected E element;
+    protected TreeNode<E> left;
+    protected TreeNode<E> right;
+
+    public TreeNode(E e) {
+        element = e;
+    }
+}
